5b6fd5df9e13d0cbdd8bbcdffdbbe2d1c3c25302
Léo Garret <leogarret@hotmail.com>
2019-12-12 16:45:48 +0100

    Remove CGAL

--- Console App/main.cpp
+++ Console App/main.cpp
@@ -12,22 +12,6 @@ using namespace mc;
 
 int main()
 {
-	mvcg::Mesh mesh;
-	mvcg::open_obj(mesh, "../../obj/Sphere-632586.obj");
-	mvcg::intersection::IntersectionData datas;
-	vcg::Ray3d ray;
-	ray.Set(vcg::Point3d(-40, -20, 30), vcg::Point3d(0, 0, -30));
-
-	mesh.TreeMake();
-
-	mvcg::intersection::Intersect(mesh, datas, ray);
-	
-	for (auto elem : datas.pointsIntersections)
-	{
-		std::cout << elem.X() << " " << elem.Y() << " " << elem.Z() << std::endl;
-	}
-
-	mvcg::save_obj(mesh, "OUT.obj");
 
 	std::getchar();
 	return 0;
